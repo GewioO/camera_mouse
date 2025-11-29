d457e5e3a6a6c5aa5eca1e356786901f4b0bdd1d
Gewi <work.gewi@gmail.com>
2025-11-29 21:29:57 +0200

    Add configurations for CLI

--- cli_manager.py
+++ cli_manager.py
@@ -6,13 +6,19 @@ class CLIManager:
     def __init__(self, json_manager: JsonManager | None = None):
         self.json_manager = json_manager or JsonManager()
 
+        self.profiles = self.json_manager.load_profiles()
+        
+        profile_names = list(self.profiles.keys())
+        fixed_modes = ["help", "configuration"]
+        all_modes = sorted(list(set(profile_names + fixed_modes)))
+        
         self.parser = argparse.ArgumentParser(add_help=False)
         self.parser.add_argument(
             "mode",
             nargs="?",
             default=None,
-            choices=["default", "touch", "scroll", "help", "configuration"],
-            help=""
+            choices=all_modes,
+            help="profile or command"
         )
         self.parser.add_argument(
             "--lang",
@@ -23,7 +29,6 @@ class CLIManager:
         self.args = self.parser.parse_args()
 
         self.texts = self.json_manager.load_texts()
-        self.profiles = self.json_manager.load_profiles()
         self.main_config = self.json_manager.load_main_config()
 
         self.lang = self.args.lang or self.main_config.get("lang", "uk")
@@ -32,13 +37,16 @@ class CLIManager:
             self.mode = self.args.mode
         else:
             stored_mode = self.main_config.get("last_profile", "default")
-            if stored_mode == "help":
-                stored_mode = "default"
+            if stored_mode == "help" or stored_mode not in self.profiles:
+                stored_mode = next((name for name in self.profiles.keys() if name != "help"), "default")
             self.mode = stored_mode
 
-        self.current_profile = self.profiles.get(self.mode, self.profiles.get("default", {}))
+        if self.mode in self.profiles:
+            self.current_profile = self.profiles[self.mode]
+        else:
+            self.current_profile = {}
 
-        if self.mode != "help":
+        if self.mode in self.profiles:
             self.main_config["last_profile"] = self.mode
         self.main_config["lang"] = self.lang
         self.json_manager.save_main_config(self.main_config)
@@ -54,3 +62,8 @@ class CLIManager:
 
     def persist_state(self) -> None:
         self.json_manager.save_main_config(self.main_config)
+
+    @property
+    def available_modes(self) -> list:
+        profile_modes = list(self.profiles.keys())
+        return sorted(profile_modes + ["help", "configuration"])
